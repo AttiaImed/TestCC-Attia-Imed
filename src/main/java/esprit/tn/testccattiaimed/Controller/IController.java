3c4143cefffc3f208e403ffd3fdcde299132dc01
ImeedAttia <attia.imeed@gmail.com>
2024-02-16 11:43:51 +0100

    second commit

---
+++ src/main/java/esprit/tn/testccattiaimed/Controller/IController.java
@@ -0,0 +1,7 @@
+package esprit.tn.testccattiaimed.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IController {
+}
--- src/main/java/esprit/tn/testccattiaimed/TestCcAttiaImedApplication.java
+++ src/main/java/esprit/tn/testccattiaimed/TestCcAttiaImedApplication.java
@@ -2,7 +2,9 @@ package esprit.tn.testccattiaimed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class TestCcAttiaImedApplication {
 
