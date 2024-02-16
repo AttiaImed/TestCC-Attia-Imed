42a43c8d2b84380c6723d5c1f50d095b84f0ba71
ImeedAttia <attia.imeed@gmail.com>
2024-02-16 11:13:54 +0100

    First Commit

---
+++ src/main/java/esprit/tn/testccattiaimed/TestCcAttiaImedApplication.java
@@ -0,0 +1,13 @@
+package esprit.tn.testccattiaimed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCcAttiaImedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCcAttiaImedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esprit/tn/testccattiaimed/TestCcAttiaImedApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.tn.testccattiaimed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCcAttiaImedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
